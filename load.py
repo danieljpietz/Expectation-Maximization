beefe38803fbc81d4df37584fdb42f1dec0415fd
danieljpietz00 <daniel@orca.engineering>
2021-11-10 17:36:34 -0700

    Initial Commit

---
+++ load.py
@@ -0,0 +1,60 @@
+import numpy as np
+from mnist import MNIST
+
+
+# Load the data from the local MNIST path
+def load_dataset():
+    global X_train, X_test, labels_train, labels_test, d
+    mndata = MNIST('MNIST')
+    d = 784
+    X_train, labels_train = map(np.array, mndata.load_training())
+    X_test, labels_test = map(np.array, mndata.load_testing())
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+def one_hot_encode(classes=None, garbage=False):
+    global labels_train_mat, labels_test_mat, X_train, X_test, labels_train, labels_test, d
+
+    if classes is None:
+        labels_train_mat = np.zeros((np.size(labels_train), 10))
+        labels_test_mat = np.zeros((np.size(labels_test), 10))
+        for i in range(0, np.size(labels_train)):
+            labels_train_mat[i][labels_train[i]] = 1
+        for i in range(0, np.size(labels_test)):
+            labels_test_mat[i][labels_test[i]] = 1
+    else:
+        train_bad_inds = []
+        test_bad_inds = []
+        if garbage:
+            listLen = len(classes) + 1
+        else:
+            listLen = len(classes)
+        labels_train_mat = np.zeros((0, listLen))
+        labels_test_mat = np.zeros((0, listLen))
+        for i in range(0, np.size(labels_train)):
+            num = labels_train[i]
+            label_temp = [0 for i in range(listLen)]
+            if num in classes:
+                label_temp[classes.index(num)] = 1
+            elif garbage:
+                label_temp[-1] = 1
+            else:
+                train_bad_inds.append(i)
+            labels_train_mat = np.vstack([labels_train_mat, label_temp])
+        X_train = np.delete(X_train, train_bad_inds, axis=0)
+        for i in range(0, np.size(labels_test)):
+            num = labels_test[i]
+            label_temp = [0 for i in range(listLen)]
+            if num in classes:
+                label_temp[classes.index(num)] = 1
+            elif garbage:
+                label_temp[-1] = 1
+            else:
+                test_bad_inds.append(i)
+            labels_test_mat = np.vstack([labels_test_mat, label_temp])
+        X_test = np.delete(X_test, test_bad_inds, axis=0)
+
+
+def load_and_encode(classes=None, garbage=False):
+    load_dataset()
+    one_hot_encode(classes, garbage)
---
+++ main.py
@@ -0,0 +1,9 @@
+import load as ld
+import numpy as np
+
+def main():
+    ld.load_and_encode(classes=[1, 2], garbage=False)
+
+
+if __name__ == "__main__":
+    main()
